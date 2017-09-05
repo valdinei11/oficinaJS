1ab1ec1f30e4b031b128e69578a6e9562b34cf0c
Valdinei Ferreira <valdinei_vof@hotmail.com>
2017-09-04 21:44:01 -0300

    Desenvolvimento do algoritmo de CPF #02
    * Instruções para programação do algoritmo
    * Algoritmo implementado utilizando JS e HTML

--- js/formValid.js
+++ js/formValid.js
@@ -1,19 +1,27 @@
 function validarCPF(cpf) {
     var soma = 0;
-    var resto;
+    var resto = 0;
 
+    // Para não haver falha no cálculo, o CPF abaixo torna-se automaticamente inválido
     if (cpf == "00000000000"){
         return false;
     }
 
+    // CALCULANDO E VERIFICANDO PRIMEIRO DÍGITO
+
+    // Como declarado no algoritmo, cada um dos 9 primeiros dígitos do CPF serão multiplicados por 10 a 2, da esquerda para a direita.
+    // No laço de repetição abaixo, a função SUBSTRING separa os caracteres do CPF, multiplica pelo seu respectivo peso (de 10 a 2) e por fim é adicionado ao valor total da soma, armazenado na variável SOMA
 	for (i=1; i<=9; i++) {
         soma = soma + parseInt(cpf.substring(i-1, i)) * (11 - i);
     }
 
-	resto = (soma * 10) % 11;
+    // O resultado (a soma) deve ser dividido por 11 e o resto da divisão será armazenado em uma variável
+	resto = soma % 11;
 	
-    if ((resto == 10) || (resto == 11)) {
+    if (resto < 2) {
         resto = 0;
+    } else {
+        resto = 11-resto;
     }
 
     if (resto != parseInt(cpf.substring(9, 10))) {
@@ -21,15 +29,18 @@ function validarCPF(cpf) {
     }
 
     soma = 0;
-    
+
+    // CALCULANDO E VERIFICANDO SEGUNDO DÍGITO
     for (i = 1; i <= 10; i++) {
         soma = soma + parseInt(cpf.substring(i-1, i)) * (12 - i);
     }
 
-    resto = (soma * 10) % 11;
+    resto = soma % 11;
 	
-    if ((resto == 10) || (resto == 11)) {
+    if (resto < 2) {
         resto = 0;
+    } else {
+        resto = 11-resto;
     }
 
     if (resto != parseInt(cpf.substring(10, 11))) {
@@ -62,7 +73,7 @@ function validar() {
     } else if (cpf.length < 11) {
         alert("Campo 'CPF' com formato inválido!");
         return false;
-    } else if (validarCPF) {
+    } else if (!validarCPF(cpf)) {
         alert("CPF Inválido!");
         return false;
     }
--- readme.md
+++
@@ -1,4 +0,0 @@
-# Guia da Oficina de JavaScript
-
-## Introdução
-Para facilitar o aprendizado, é recomendado que tenha conhecimentos em lógica de programação e em HTML5.
