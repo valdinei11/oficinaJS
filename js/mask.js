07fb80d6f3e4792c9426aed18bcfbf42f7d4662a
Valdinei Ferreira <valdinei_vof@hotmail.com>
2017-09-18 00:30:45 -0300

    doc e organiz de arquivos de jQuery masks

--- js/mask.js
+++ js/mask.js
@@ -1,3 +1,4 @@
 jQuery(function($){
     $("#cpf").mask("999.999.999-99");
 });
+// No campo com ID igual a 'cpf', o número 9 é substituido por '_' e será inserida uma máscara do tipo: ___.___.___-__
